5dfdbeb207acc7d5c86c07f2ef1e8d2dff34ae71
Aiden Foxx <aiden.foxx.mail@gmail.com>
2016-12-04 00:12:08 +0000

    Fixed gitignore

--- vendor/libarchive-3.2.2/mingw/libarchive/CMakeFiles/archive_static.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libarchive_static.a"
-)
--- vendor/libarchive-3.2.2/mingw/libarchive/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Development/Other/Projects/foxx-engine/vendor/libarchive-3.2.2/libarchive
-# Build directory: D:/Development/Other/Projects/foxx-engine/vendor/libarchive-3.2.2/mingw/libarchive
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(test)
